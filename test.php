682055ea7745d10e9f9070d17edc89c218cf2bdd
Martin Stücklschwaiger <martin.stuecklschwaiger@walls.io>
2020-07-29 21:29:14 +0200

    Also fine

--- test.php
+++ test.php
@@ -2,17 +2,21 @@
 
 declare(strict_types=1);
 
-function blah($input)
+function blah($input, $other)
 {
     switch ($input) {
       case 'one':
+        if ($other === 'one') {
+            return 'real_one';
+        }
+
         return 'blah_one';
       default:
         return 'blah_default';
     }
 }
 
-$x = blah('two');
+$x = blah('two', 'something');
 var_dump($x);
 
 echo "\n";
